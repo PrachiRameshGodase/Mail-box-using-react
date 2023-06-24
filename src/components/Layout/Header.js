25c7ddec9dc5d3ef4aa305376619f9b2faf1975b
PrachiRameshGodase <prachigodase2@gmail.com>
2023-06-24 19:33:07 +0530

    adding mailbox

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,18 +3,40 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import classes from "./Header.module.css"
+import { useDispatch,useSelector } from 'react-redux';
+import { authActions } from '../../store/auth';
+import { useNavigate } from 'react-router-dom';
 
 
 function Header() {
+    const dispatch = useDispatch();
+    const navigate=useNavigate()
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+    const logOutHandler=()=>{
+    dispatch(authActions.logout());
+    navigate("/");
+    }
+ 
   return (
     <div>
     <Navbar expand="lg" className="bg-body-tertiary bg-gradient-to-r from-red-900 via-yellow-200 to-green-400 ">
       <Container>
         <Navbar.Brand href="#home" className={`mr-8 text-gray-100 font-bold bg-gradient-to-r from-blue-800 to-yellow-50 ${classes.logo} `}>MAIL BOX</Navbar.Brand>
         
-        <Link to="/">
+        {!isAuth && (<Link to="/">
               <button className='bg-gradient-to-b from-red-600 via-red-500 to-red-800  hover:bg-purple-600 py-2 px-4 font-bold text-white rounded'>LOGIN</button>
-            </Link>
+        </Link>)}
+
+        {isAuth && (
+            <button
+              className='bg-gradient-to-b from-red-600 via-red-500 to-red-800  hover:bg-purple-600 py-2 px-4 font-bold text-white rounded mx-5 '
+              onClick={logOutHandler}
+            >
+              LOGOUT
+            </button>
+        )}
+
         </Container> 
     </Navbar>
 
--- src/components/Pages/MailBox.js
+++ src/components/Pages/MailBox.js
@@ -1,11 +1,97 @@
-import React from 'react'
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
 
 function MailBox() {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const subjectChangeHandler = (event) => {
+    setSubject(event.target.value);
+  };
+
+  const sendEmailHandler = (event) => {
+    event.preventDefault();
+
+    if (!email.includes("@") || !email.includes(".")) {
+        console.log("Invalid email");
+        return;
+      }
+    // const enteredEmail=localStorage.getItem("email")
+    const emailContent = editorState.getCurrentContent().getPlainText();
+    const changedemail = email.replace("@", "").replace(".", "");
+    console.log('Recipient:', email);
+    console.log('Changed mail:', changedemail);
+    console.log('Subject:', subject);
+    console.log('Email Content:', emailContent);
+
+    const newData = {
+      email: emailContent,
+      subject: subject,
+      
+    };
+            // Send a POST request to store the new expense in the database
+    fetch(`https://mailbox-http-default-rtdb.firebaseio.com/user/${changedemail}.json`, {
+        method: 'POST',
+        body: JSON.stringify(newData),
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then((response) => {
+          console.log('Email successfully send');
+          console.log(response)
+          if (response.ok) {
+            return response.json(); 
+          } else {
+            throw new Error('Failed to send email');
+          }
+        })
+        .then((data) => { 
+          console.log(data);
+            setEmail('');
+            setSubject('');
+            setEditorState(EditorState.createEmpty());
+         
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+
+    
+  };
+
   return (
-    <div>
-      <h1>Welcome to your MailBox!</h1>
-    </div>
-  )
+    <form onSubmit={sendEmailHandler} className="w-full mt-8 bg-green-100 p-8 shadow-md " style={{marginRight:"100px"}}>
+      <div className="mb-4">
+        <label className="block text-gray-700">To</label>
+        <input type="email" value={email} onChange={emailChangeHandler} className="form-input mt-1 block w-full py-2 px-2 hover:bg-red-50 rounded" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700">Subject</label>
+        <input type="text" value={subject} onChange={subjectChangeHandler} className="form-input mt-1 block w-full py-2 px-2 hover:bg-red-50 rounded" />
+      </div>
+      <div className="mb-4 mt-5">
+        <Editor
+          editorState={editorState}
+          toolbarClassName="flex"
+          wrapperClassName="bg-white rounded p-3 focus:outline-none"
+          editorClassName="w-full h-full"
+          onEditorStateChange={onEditorStateChange}
+        />
+      </div>
+      <Button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">Send</Button>
+    </form>
+  );
 }
 
-export default MailBox
+export default MailBox;
